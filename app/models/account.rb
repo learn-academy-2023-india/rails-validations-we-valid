74f0d096f9041a08996d8f4a0b8d8a504d99436d
Sakaa4Short <klanyenek@gmail.com>
2024-01-24 15:40:58 -0800

    Correction Commit

---
+++ app/models/account.rb
@@ -0,0 +1,7 @@
+class Account < ApplicationRecord
+    # validates :username, :password, :email presence: true
+    # validates :username, uniqueness: true,
+    # validates :username, length: { minimum: 5 } 
+    # validates :password, uniqueness: true
+    # validates :password, length: { minimum: 6 }
+end
